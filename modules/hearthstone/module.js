f77fec83f0d4324d64632fa796f7bb04e7eb5de7
StijnDotExe <stijn.exe@gmail.com>
2018-05-28 19:20:00 +0200

    First draft of !hs

--- modules/hearthstone/module.js
+++ modules/hearthstone/module.js
@@ -1,17 +1,32 @@
-function getCard(cardName){
+function getCard(msg, config) {
+    msg.channel.send("Searching...");
+    var cardName = msg.content.substring(msg.content.indexOf(" ") + 1);
     var options = {
-        url: "https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/" + cardName,
+        url: "https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/"
+            + cardName
+            + "?collectible=1",
         headers: {
             "X-Mashape-Key": config.hearthstone.hearthstone_api_key
         }
-      }
+    }
     config.hearthstone.request.get(options, (error, response, body) => {
-        console.log(error);
-        console.log(response);
-        console.log(body);
+        var cards = JSON.parse(body);
+        // If no cards are found, reply that
+        if (cards.error != undefined) {
+            if (cards.error == 404) msg.reply("no cards with the name \"" + cardName + "\"!");
+        }
+        // If the specific card is found, return it
+        else if (cards.length == 1) {
+            msg.reply(cards[0].imgGold);
+        }
+        else if (cards.length > 1) {
 
-        return "KAART INFO";
+        }
     });
+}
+
+function askForMoreInfo(){
+
 }
 
 module.exports = {
@@ -20,17 +35,15 @@ module.exports = {
         var commands = [
             {
                 command: "hs",
-                reply: function ping(msg) {
-                    msg.reply(
-                        // Get card based on cardname (leave away {prefix}hs)
-                        getCard(msg.content.substring(msg.content.indexOf(" ")))
-                    );
+                reply: function hs(msg, config) {
+                    // Get card based on cardname (leave away {prefix}hs)
+                    getCard(msg, config)
                 }
             }];
         return commands;
     },
 
-    getConfig(){
+    getConfig() {
         return {
             "request": require("request"),
             "hearthstone_api_key": "GET_FROM_CONFIG"  // To be defined in discat/MDK
